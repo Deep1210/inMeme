0c9db5d63769f023ecfdb67f051105f88847dbb5
Abhishek <apsrkgitcs@gmail.com>
2020-01-12 19:57:36 +0530

    downlaod image

--- app/screens/home/home_screen.js
+++ app/screens/home/home_screen.js
@@ -21,6 +21,7 @@ import {
     TouchableOpacity
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Download} from '../../utils';
 
 const SCREEN_HEIGHT = Dimensions.get("window").height
 const SCREEN_WIDTH = Dimensions.get("window").width
@@ -41,7 +42,8 @@ export default class HomeScreen extends Component {
         this.state = {
             pan: new Animated.ValueXY(),
             currentIndex: 0,
-            show: false
+            show: false,
+            memeData:[]
         }
         this.state.panResponder = PanResponder.create({
             onStartShouldSetPanResponder: () => true,
@@ -70,10 +72,34 @@ export default class HomeScreen extends Component {
         });
     }
 
+    componentDidMount() {
+        this.getMemes()
+    }
+
+    getMemes() {
+        fetch('http://207.246.125.54/api/meme?deviceId=12345', {
+            method: 'GET'
+        }).then(response=>{
+            console.log('response',response)
+            if(response.status==200){
+                return response.json()
+            }
+        }).then(responseJson=>{
+            console.log('responseJson',responseJson)
+            this.setState({memeData:responseJson.results})
+        })
+    }
+
+    downloadImage(){
+        let image_url=this.state.memeData[this.state.currentIndex].avatar
+        Download.permission()
+        Download.start(image_url)
+    }
+
 
     renderArticles = () => {
 
-        return ARTICLES.map((item, i) => {
+        return this.state.memeData.map((item, i) => {
             console.log('index', i, "pan", this.state.pan)
             if (i < this.state.currentIndex) {
                 return null
@@ -89,7 +115,7 @@ export default class HomeScreen extends Component {
                             width: SCREEN_WIDTH
                         }}>
                             <View style={{ flex: 2 }}>
-                                <Image source={ARTICLES[i].uri}
+                                <Image source={{uri:item.avatar}}
                                     style={{ flex: 1, height: null, width: null, resizeMode: 'center', backgroundColor: 'black' }} />
                             </View>
                         </View>
@@ -106,7 +132,7 @@ export default class HomeScreen extends Component {
                             width: SCREEN_WIDTH
                         }}>
                             <View style={{ flex: 2 }}>
-                                <Image source={ARTICLES[i].uri}
+                                <Image source={{uri:item.avatar}}
                                     style={{ flex: 1, height: null, width: null, resizeMode: 'center', backgroundColor: 'black' }} />
                             </View>
 
@@ -117,7 +143,8 @@ export default class HomeScreen extends Component {
         }).reverse()
     }
 
-    showHide() {
+    showHide(event) {
+        console.log('event',event)
         this.setState({ show: !this.state.show })
     }
 
@@ -130,7 +157,7 @@ export default class HomeScreen extends Component {
         return (
             <View style={{ flex: 1 }}>
                 {this.state.show ?
-                    <View style={{ backgroundColor: 'white', flex: 1,justifyContent:'center' }}>
+                    <View style={{ backgroundColor: 'white', flex: 1, justifyContent: 'center' }}>
                         <TouchableOpacity onPress={() => this.openDrawer()}>
                             <Icon size={25} name={'dehaze'} />
                         </TouchableOpacity>
@@ -138,13 +165,13 @@ export default class HomeScreen extends Component {
                     :
                     (null)
                 }
-                <View style={{ flex: 12 }} onTouchEndCapture={() => this.showHide()}>
+                <View style={{ flex: 12 }} onTouchEndCapture={(event) => this.showHide(event)}>
                     {this.renderArticles()}
                 </View>
-                {this.state.show ? <View style={{ backgroundColor: 'white', flex: 1, flexDirection: 'row',justifyContent:'space-evenly',alignItems:'center' }}>
+                {this.state.show ? <View style={{ backgroundColor: 'white', flex: 1, flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center' }}>
                     <Icon size={25} name={'favorite-border'} />
-                    <Icon size={25} name={'file-download'} />
-                    <Icon size={25} name={'share'} />
+                    <TouchableOpacity onPress={()=>this.downloadImage()}><Icon size={25} name={'file-download'} /></TouchableOpacity>
+                   <TouchableOpacity ><Icon size={25} name={'share'} /></TouchableOpacity>
                 </View> : (null)}
             </View>
         )
